3dad4ef1f5f3e6163f0086d11992f396cb36b86c
Dongmin88 <47022094+Dongmin88@users.noreply.github.com>
2020-09-16 19:48:04 +0900

    Create GamePoints.cs

---
+++ GamePoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePoints : MonoBehaviour
+{
+    public int Gold;
+    public int Success;
+    public int Fail;
+    public int Day;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gold = 0;
+        Success = 0;
+        Fail = 0;
+        Day = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
